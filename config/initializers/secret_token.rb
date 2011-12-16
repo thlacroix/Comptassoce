65d2202acba99d058a3b1023409c3f8d2206ec9a
Psytom <comptassoce@larez.fr>
2011-12-16 16:10:33 +0100

    Commit de l'apli

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comptassoce::Application.config.secret_token = '68d931b0ef9691b92d188e503673315ed3b1fc9ccb39277142e254907b67fe053702926388c5c3776d1e0d01fb1f63983a12ab0ef4b6d845d64e49eb56fd5a26'
